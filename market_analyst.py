1a681c768ea47e56dc434374a909397c7c17ffa9
DeekshaBPoojary <deekshabpoojary@gmail.com>
2025-04-28 13:00:45 +0530

    Delete market_analyst.py

--- market_analyst.py
+++
@@ -1,8 +0,0 @@
-class SoilAnalyzer:
-    def analyze(self, soil_type):
-        if soil_type == "Sandy":
-            return ["Pearl Millet", "Groundnut"]
-        elif soil_type == "Clay":
-            return ["Wheat", "Rice"]
-        else:
-            return ["Wheat", "Soybean"]
