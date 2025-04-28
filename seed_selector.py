d8858b2f8613d62ef6234e28b3035934166eca2b
CHARITHA KR <charitha123103@gmail.com>
2025-04-28 23:52:49 +0530

    Delete seed_selector.py

--- seed_selector.py
+++
@@ -1,9 +0,0 @@
-# agents/seed_selector.py
-
-class SeedSelector:
-    def select_best_seed(self, soil_output, weather_output, market_output, sustainability_output):
-        # Dummy logic for now
-        final_crop = market_output["best_crop"]
-        return {
-            "final_recommendation": final_crop
-        }
