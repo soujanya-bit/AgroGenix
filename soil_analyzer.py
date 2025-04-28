a08b819d1b1a825446ffe58594f4530c68af35af
CHARITHA KR <charitha123103@gmail.com>
2025-04-28 23:55:00 +0530

    Delete soil_analyzer.py

--- soil_analyzer.py
+++
@@ -1,9 +0,0 @@
-# agents/soil_analyzer.py
-
-class SoilAnalyzer:
-    def analyze(self, soil_type, moisture, rainfall):
-        # Dummy logic for now
-        return {
-            "recommended_crops": ["Wheat", "Soybean", "Maize"],
-            "soil_score": 0.85
-        }
