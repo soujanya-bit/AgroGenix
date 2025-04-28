c372f0789d66ba501dc94c544717d2c6bbbf1432
CHARITHA KR <charitha123103@gmail.com>
2025-04-28 23:55:33 +0530

    Delete weather_agent.py

--- weather_agent.py
+++
@@ -1,9 +0,0 @@
-# agents/weather_agent.py
-
-class WeatherAgent:
-    def assess_weather(self, season, rainfall_level):
-        # Dummy logic for now
-        return {
-            "weather_risk": "Low",
-            "weather_score": 0.90
-        }
